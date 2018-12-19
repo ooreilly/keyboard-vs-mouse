7b036fc772bc3e1df8dc5e8197a85f8412bafe04
ooreilly <ooreilly@usc.edu>
2018-12-18 20:56:59 -0800

    Fix client so that it works on linux.

--- keyboardvsmouse.py
+++ keyboardvsmouse.py
@@ -7,12 +7,13 @@ The first time you use this client you must edit `config.py`. Open this file
 and follow the instructions there.
 
 """
-#import keyboard
+import keyboard as unix_keyboard
 from pynput import mouse, keyboard
 import time
 import config as cfg
 import stats
 import client
+import platform
 
 cli = client.Client(cfg.host, cfg.login)
 cli.authorize()
@@ -58,13 +59,21 @@ def on_click(x, y, button, pressed):
 def on_scroll(x, y, dx, dy):
     pass
 
-with keyboard.Listener(
-        on_release=on_release
-        ) as listener:
+if platform.system() == 'Linux':
+    unix_keyboard.on_release(on_release)
     with mouse.Listener(
         on_click=on_click,
         on_move=on_move
         ) as mlistener:
-        listener.join()
         mlistener.join()
-
+else:
+    with keyboard.Listener(
+            on_release=on_release
+            ) as listener:
+        with mouse.Listener(
+            on_click=on_click,
+            on_move=on_move
+            ) as mlistener:
+            listener.join()
+            mlistener.join()
+    
